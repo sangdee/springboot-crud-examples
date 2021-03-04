58874ae2ad7b9c2b11d12ce4dcf8ccb7d4312abf
sangdee <rhkcnf45>
2021-03-04 21:45:09 +0900

    initial commit

---
+++ fortunecoin-web/src/main/java/com/gwell/fortunecoinweb/FortunecoinWebApplication.java
@@ -0,0 +1,13 @@
+package com.gwell.fortunecoinweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortunecoinWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FortunecoinWebApplication.class, args);
+    }
+
+}
---
+++ fortunecoin-web/src/test/java/com/gwell/fortunecoinweb/FortunecoinWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwell.fortunecoinweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortunecoinWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
